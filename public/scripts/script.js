1b8c6c161e50635479c0f90db3ec0afe43bf02a9
chillbuilds <will@chillbuilds.com>
2024-03-12 16:22:04 -0600

    added barcode loader

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,15 +1,42 @@
-let generateBarcodes = (barcodeArr) => {
-    barcodeArr.forEach(barcode => {
-        if(barcode == ''){
-            return
-        }else{
-            let barcodeWidth = localStorage.getItem('barcode-width')
-            let barcodeHeight = localStorage.getItem('barcode-height')
-            let barcodeMargin = localStorage.getItem('barcode-margin')
-            $('#barcode-container').append(`<img class="barcode-img" id="barcode-${barcode}">`)
-            $('.barcode-img').attr('style', `width: ${barcodeWidth}px; height: ${barcodeHeight}px; padding-left: ${barcodeMargin}px;`)
-            JsBarcode(`#barcode-${barcode}`, `${barcode}`)
+async function generateBarcodes(barcodeArr) {
+
+    if(barcodeArr[barcodeArr.length-1] == ''){
+        barcodeArr.pop()
+    }
+
+    $('#totalBarcodes').text(barcodeArr.length.toString())
+
+    // Split the processing into chunks to prevent blocking
+    const chunkSize = 1;
+    for (let i = 0; i < barcodeArr.length; i += chunkSize) {
+        $('#completedBarcodes').text(i+1)
+        const chunk = barcodeArr.slice(i, i + chunkSize)
+        await processBarcodeChunk(chunk)
+        if(i == barcodeArr.length - 1){
+            $('#barcode-container').attr('style', 'opacity:1;')
+            $('.spinner-bg').attr('style', 'display:none;')
+            $('.spinner-container').attr('style', 'display:none;')
         }
+    }
+}
+
+async function processBarcodeChunk(chunk) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            chunk.forEach(barcode => {
+                if (barcode === '') {
+                    return
+                } else {
+                    const barcodeWidth = localStorage.getItem('barcode-width')
+                    const barcodeHeight = localStorage.getItem('barcode-height')
+                    const barcodeMargin = localStorage.getItem('barcode-margin')
+                    $('#barcode-container').append(`<img class="barcode-img" id="barcode-${barcode}">`)
+                    $('.barcode-img').attr('style', `width: ${barcodeWidth}px; height: ${barcodeHeight}px; padding-left: ${barcodeMargin}px;`)
+                    JsBarcode(`#barcode-${barcode}`, `${barcode}`)
+                }
+            });
+            resolve()
+        }, 0)
     })
 }
 
@@ -35,11 +62,39 @@ let checkLocalStorage = () => {
     $('#barcode-margin-input').val(localStorage.getItem('barcode-margin'))
 }
 
+async function processCSV(file) {
+    return new Promise((resolve, reject) => {
+        $('#barcode-container').attr('style', 'opacity:0.2;')
+        $('.spinner-bg').attr('style', 'display:block;')
+        $('.spinner-container').attr('style', 'display:block;')
+
+        var reader = new FileReader()
+        reader.onload = function (e) {
+            var csvData = e.target.result
+            csvData = csvData.split(' ').join('')
+            csvData = csvData.split('\r').join('')
+            csvData = csvData.split('\n').join(',')
+            if (csvData.length < 1 || csvData == ' ') {
+                alert('csv file is empty')
+                return
+            }
+            generateBarcodes(csvData.split(','))
+                .then(() => resolve())
+                .catch(error => reject(error))
+        }
+        reader.onerror = function (event) {
+            reject(event.target.error)
+        }
+
+        reader.readAsText(file)
+    })
+}
+
 $(document).ready(function(){
 
     checkLocalStorage()
 
-    $('#barcode-input').change(function(e){
+    $('#barcode-input').change(async function(e){
         var file = e.target.files[0]
         if (!file) {
             return
@@ -49,20 +104,12 @@ $(document).ready(function(){
             alert('selected file is not a csv')
             return
         }
-        
-        var reader = new FileReader()
-        reader.onload = function(e){
-            var csvData = e.target.result
-            csvData = csvData.split(' ').join('')
-            csvData = csvData.split('\r').join('')
-            csvData = csvData.split('\n').join(',')
-            if(csvData.length < 1 || csvData == ' '){
-                alert('csv file is empty')
-                return
-            }
-            generateBarcodes(csvData.split(','))
+
+        try {
+            await processCSV(file)
+        } catch (error) {
+            console.error('Error processing CSV:', error)
         }
-        reader.readAsText(file)
     })
 
     $('.barcode-size-input').on('input', function() {
